591fb124f628174daa35b050cdef09995c2cb5cf
madprogramer <themadprogramer@gmail.com>
2021-07-07 15:43:33 +0300

    updated scoping
    TODO: Add Tutorial on how to set up cookies, headers and configure crontab

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import requests,json; 
-with open("cookies.json",'r') as cookies:
-  with open("headers.json",'r') as headers:
+import requests,json,os,sys;
+
+PATH = os.path.dirname(sys.argv[0])
+with open(os.path.join(PATH,"cookies.json"),'r') as cookies:
+  with open(os.path.join(PATH,"headers.json"),'r') as headers:
     r=requests.get("https://eksisozluk.com/",cookies=json.loads(cookies.read()),headers=json.loads(headers.read()))
     #r=requests.get("https://eksisozluk.com/")
     print(r)
